a5555834bcf76672044fcd07be8c47d07e45a8fa
MbaliMt <mz.mthimkhulu@gmail.com>
2020-10-14 02:04:41 +0200

    modified task 5

--- Task5.py
+++ Task5.py
@@ -1,5 +1,12 @@
-def areaOfTriangle(half, height, base):
-    area = half*(base * height)
-    return area
 
-print("The area of a triangle is:",areaOfTriangle(0.5 , 5, 7))
+def areaOfTriagle(a, b, c):
+    semi = a + b + c
+    semi = semi / 2
+
+    areaT = float((semi * (semi - float(a)) * (semi - float(b)) * (semi - float(c))) ** 0.5)
+    return areaT
+
+
+print(areaOfTriagle(3, 4, 5))
+
+
