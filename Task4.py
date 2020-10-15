334be44f440a358855ed8e229344c217f6a264ca
MbaliMt <mz.mthimkhulu@gmail.com>
2020-10-15 12:17:38 +0200

    made changes to task 4

--- Task4.py
+++ Task4.py
@@ -2,12 +2,12 @@
 def twoNumbers(num1, num2):
     sum = num1 + num2
     sumToList = list(map(int, str(sum)))
-    print(sumToList)
+    #print(sumToList)
     print(sum)
-    if num1 == 3 or num2 == 3 or 3 in sumToList:
+    if num1 == 3 or num2 == 3 and 3 in sumToList:
         return True
     else:
         return False
 
-print(twoNumbers(126, 4))
+print(twoNumbers(3, 70))
 
